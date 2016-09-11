162a49f9ed535d2bb90da4024bae6670e1b3445b
Richard Kimber <richard@dogma.co.uk>
2016-09-11 19:09:41 +0100

    Reset creds

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var client = require('twilio')('ACbfe1068ee9da0ee20cf72aabc93159c8', '6fd35273118ca42fbd62d75d7bbbb843');
+var client = require('twilio')('', '');
 var dash_button = require('node-dash-button');
 
 var dash = dash_button('44:65:0d:66:87:c4'); //address from step above
