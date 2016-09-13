2649514f346f42e3814c4875092b926b77e9c480
Richard Kimber <richard@dogma.co.uk>
2016-09-13 22:41:38 +0100

    added log

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ var accountSid = process.argv[2],
 	numbers = process.argv[4].split(','),
 	fromNumber = process.argv[5];
 
+var fs = require('fs');
+
 var client = require('twilio')(accountSid, authToken);
 var dash_button = require('node-dash-button');
 
 var dash = dash_button('44:65:0d:66:87:c4', null, null, 'all'); //address from step above
+
+function log(message) {
+	fs.appendFileSync('log.txt', '[' + Date() + '] ' + message);
+}
 
 function notify(number) {
 	client.messages.create({
@@ -14,7 +20,7 @@ function notify(number) {
 		to: number,
 		from: fromNumber
 	}, function() {
-		//console.log(arguments);
+		log(number + ' notified.');
 	});
 }
 
@@ -26,4 +32,4 @@ dash.on('detected', function (){
 	}
 });
 
-console.log('running');
+log('Script running.');
