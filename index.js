783ec570672c98240732d8b409d198dcb041ce59
Richard Kimber <richard@dogma.co.uk>
2016-09-13 21:53:37 +0100

    Arguments

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-var client = require('twilio')(process.argv[2], process.argv[3]);
+var accountSid = process.argv[2],
+	authToken = process.argv[3],
+	numbers = process.argv[4].split(','),
+	fromNumber = process.argv[5];
+
+var client = require('twilio')(accountSid, authToken);
 var dash_button = require('node-dash-button');
 
 var dash = dash_button('44:65:0d:66:87:c4', null, null, 'all'); //address from step above
-
-var numbers = process.argv[4].split(',');
 
 function notify(number) {
 	client.messages.create({
 		body: 'Tea is ready',
 		to: number,
-		from: process.argv[5]
+		from: fromNumber
 	}, function() {
 		//console.log(arguments);
 	});
